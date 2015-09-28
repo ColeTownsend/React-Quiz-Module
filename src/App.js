7ec2f1beff8f608b1b13e0a5f286297e6e986d63
Cole Townsend <cole@coletownsend.com>
2015-09-28 02:12:34 -0400

    got the q/a part going.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 var Question = React.createClass({
     render: function() {
         if (this.props.question.isRight) {
-          return (<span>{this.props.question.pre} {this.props.question.answer} </span>)
+          return (<span>{this.props.question.pre} <span className="flash animated">{this.props.question.answer}</span>. </span>)
         } else {
           return (<span>{this.props.question.pre} <input onChange={this.handleChange}></input> </span>);
         }
@@ -16,33 +16,38 @@ var Question = React.createClass({
         this.props.question.isRight = true;
         console.log("question " + this.props.question.id + " is " + this.props.question.isRight);
       }
-    }
+    },
+    // getInitialState: function() {
+    //     return {
+    //       isRight: "false"
+    //     }
+    // }
 });
 
 var QuestionSet = React.createClass({
     render: function() {
         var allQs = []
         this.props.questions.forEach((question) => {
-          if (!question.isRight) {
-            allQs.push(<Question question={question} key={question.id} onAnswer={this.onAnswer} />);
-          }
+          allQs.push(<Question question={question} key={question.id} onAnswer={this.onAnswer} />);
         });
 
         return (
-            <section>
-              <p>Hello</p>
-              <p>{allQs}</p>
+            <section className="container px2 mh4 flex flex-center">
+              <div className="p2">
+              <p className="serif h3 h3-responsive bold fuschia">{allQs}</p>
+              </div>
             </section>
         );
     },
     onAnswer: function() {
+      this.forceUpdate(); // going to take this out.
     }
 });
 
 
 var QUESTIONS = [
-  {id: 1, pre: 'This is my favorite type of pie:', answer: 'pumpkin pie', isRight: false},
-  {id: 2, pre: 'This is my favorite type of burger:', answer: 'public burger', isRight: false},
+  {id: 1, pre: 'This is my favorite type of pie:', answer: 'pumpkin', isRight: false},
+  {id: 2, pre: 'This is my favorite type of burger:', answer: 'public', isRight: false},
   {id: 3, pre: 'This is my favorite type of sandwich:', answer: 'turkey', isRight: true},
 ];
 
