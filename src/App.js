e21df557f3de04215e101068b920a0caeb7f7438
Cole Townsend <cole@coletownsend.com>
2015-10-09 01:02:43 -0400

    broke it but trying to get my components on other sheets.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,6 @@
 import React, { Component } from 'react';
-
-var defStyle = {
-    color: 'black',
-    fontFamily: '"SF UI Text",-apple-system, monospace',
-    lineHeight: '40px'
-};
-
-var formStyle = {
-    height: '20',
-    border: 'none',
-    borderBottom: '2px solid #04B7FF',
-    outline: 'none',
-    boxShadow: 'none',
-    margin: '0',
-    padding: '0',
-    width: '20%',
-    overflow: 'hidden',
-    paddingLeft: '10px',
-    paddingRight: '10px',
-}
-
-var Question = React.createClass({
-    getInitialState: function() {
-       return { isRight: this.props.question.isRight }
-    },
-    // handle changes in the input
-    handleChange: function(event) {
-        this.props.onAnswer();
-        if (event.target.value === this.props.question.answer) {
-            this.props.onAnswer(this.props.question.id);
-            this.setState({isRight: true});
-            console.log("question " + this.props.question.id + " is " + this.props.question.isRight);
-        }
-    },
-    render: function() {
-        if (this.state.isRight) {
-            return (<span>{this.props.question.pre} <span className="flash animated aqua">{this.props.question.answer}</span>.</span>)
-        } else {
-            return (<span>{this.props.question.pre} <input style={formStyle} onChange={this.handleChange}></input></span>);
-        }
-    }
-});
-
-        // var allQs = []
-        // this.props.questions.forEach((question) => {
-        //   allQs.push(<Question question={question} key={question.id} onAnswer={this.onAnswer} />);
-        // });
-
-var Quiz = React.createClass({
-    getInitialState: function() {
-        return {QuizQuestions: this.props.questions}
-    },
-    render: function() {
-        var allQs = []
-        this.props.questions.forEach((question) => {
-          allQs.push(<Question question={question} key={question.id} onAnswer={this.onAnswer} isRight={this.state.isRight} />);
-        });
-        return (
-          <p className="h4 h4-responsive" style={defStyle}>{allQs}</p>
-        );
-    },
-    onAnswer: function() {
-        this.setState
-    }
-});
-
+// import { Router, Route, Link } from 'react-router'
+import { Quiz } from './Quiz';
 
 var QUIZ1 = [
     {id: 1, pre: 'This is my favorite type of pie:', answer: 'pumpkin', isRight: false},
@@ -72,21 +8,17 @@ var QUIZ1 = [
     {id: 3, pre: 'This is my favorite type of sandwich:', answer: 'turkey', isRight: false},
 ];
 
-var QUIZ2 = [
-    {id: 1, pre: 'This is my favorite type of pizza:', answer: 'three cheese', isRight: false},
-    {id: 2, pre: 'This is my favorite place:', answer: 'buffalo', isRight: false},
-    {id: 3, pre: 'This is where I went to school:', answer: 'Williams', isRight: false},
-];
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Quiz questions={QUIZ1} />
+      </div>
+    );
+  }
+}
 
-var QUIZ3 = [
-    {id: 1, pre: 'Do I wear socks when I run:', answer: 'no', isRight: false},
-    {id: 2, pre: 'What is my middle name:', answer: 'Nathaniel', isRight: false},
-    {id: 3, pre: 'What was my major in college:', answer: 'art hitory', isRight: false},
-];
 
-React.render(<Quiz questions={QUIZ1} />, document.getElementById('quiz1'));
-React.render(<Quiz questions={QUIZ2} />, document.getElementById('quiz2'));
-React.render(<Quiz questions={QUIZ3} />, document.getElementById('quiz3'));
 
 
 
